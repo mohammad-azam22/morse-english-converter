a58f9f307677272d80987d8c6081c2549262d8f0
Mohammad Azam <78340188+mohammad-azam22@users.noreply.github.com>
2025-12-31 21:54:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
 from MorseEnglishConvertor import MorseEnglishConvertor
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 converter = MorseEnglishConvertor()
 
